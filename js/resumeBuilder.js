40efd77beab85cbba8e8320f27acd5733807d6a4
Nikoloz Mchedlishvili <mchedlish@yahoo.com>
2017-05-30 20:36:29 +0400

    some json

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,17 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var jobs = {
+    "Work Experience": [
+        {
+            "Employer": "ICC Georgia",
+            "Title": "Adviser to Chairman",
+            "Location": "Tbilisi, Georgia",
+            "Dates of Employment": "December 2016-present",
+            "Main Responsibilities": "Responsible for defending and lobbying interests of members of ICC Georgia. Work with the government on regulations that affect business climate"
+},
+        {
+            "Employer": "ICC Georgia",
+            "Title": "Secpnd Vice Chaorman",
+            "Location": "Tbilisi, Georgia",
+            "Dates of Employment": "May 2015-December 2016",
+            "Main Responsibilities": "Responsible for defending and lobbying interests of members of ICC Georgia. Work with the government on regulations that affect business climate"
+}]
+}
